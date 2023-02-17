daed547a2a5336059c63790e965d082f8c24639c
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-02-17 15:15:25 +0530

    String Concatination

--- Introduction/introduction.js
+++ Introduction/introduction.js
@@ -26,3 +26,61 @@ Difference between var and let:
 -> The difference between var and const is that var can be used throughout the program,
 but let can only we used inside it's scope of declaration
 */
+
+// Declaration and assignment of variables
+var a;
+a = 7;
+// here var b is declaring the variable and 2 in initialization
+var b = 2;
+b = a; // assigning value of a into b
+
+// Variables and function names are case sensitive
+// we usually use camelCase for declaring tha variables
+
+// Adding Two Numbers
+var sum = 10+12;
+// -> All other assignment operators are same
+
+// Inscrement a number
+sum++;
+sum+=1;
+sum = sum + 1;
+
+// To find remainder we use modulo operator
+sum = 10%7; // gives 3
+
+// Strings
+var myFirstName = "Saket";
+var myLastName = "Aryan";
+
+// Double Quoting - ESCAPE CHARACTERS
+var doubleQuote = "I am a \"Double Quoted\" string inside \"Double Quoted\" string."
+console.log(doubleQuote);
+
+// We can also use signle quote inside a double quote and vice versa
+doubleQuote = 'I am a "Double Quoted" string inside a \'Signle Quoted\' string.'
+
+// Another method we can use is using back ticks or `
+doubleQuote = `I am a "Double Quoted" and 'Single Quoted' string inside backticks.`
+
+// Escape Sequences in JavaScript
+
+/*****
+ 
+CODE OUTPUT
+\'  single quote
+\" double quote
+\\ backslash
+\n new line
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+
+*****/
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
+console.log(myStr)
+
+// String concatination with Plus Operator
+myStr = "This is the first string."+" This is the second string";

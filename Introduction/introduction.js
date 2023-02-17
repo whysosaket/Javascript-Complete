4302b20bec30ffba00130b02d07610cfed009af9
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-02-17 06:17:49 +0530

    Introduction to Javascript

---
+++ Introduction/introduction.js
@@ -0,0 +1,28 @@
+console.log("Hello World")
+
+// This is a comment
+/*
+This is an multiline comment
+*/
+
+// Data Types and Variables
+/*
+Types of data:
+1. undefined -> Something that hasn't been defined
+2. null -> means nothing
+3. boolean -> True/False
+4. string -> A collection of characters
+5. symbol -> A primitive value that is unique  and immutable
+6. number -> Number
+7. object -> Can store a lot of key value pairs
+*/
+
+var myName = "Saket"
+let ourName = "Aryan"
+const pi = 3.14
+
+/*
+Difference between var and let:
+-> The difference between var and const is that var can be used throughout the program,
+but let can only we used inside it's scope of declaration
+*/

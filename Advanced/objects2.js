12ec0a9727a847d43847be45154b4267d40face3
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-02-19 05:30:54 +0530

    Objects Intermediate

--- Advanced/objects2.js
+++ Advanced/objects2.js
@@ -11,3 +11,42 @@ function testLookup(a){
 }
 
 console.log(testLookup(1));
+
+// Checking if a PROPERTY exists in an OBJECT
+
+var Obj1 = {
+    "name": "Saket",
+    "age": 21,
+    "School": "ITUS"
+}
+
+console.log(Obj1.hasOwnProperty('age')) // Checking if a PROPERTY exists in an OBJECT
+console.log(Obj1.hasOwnProperty('college'))
+
+// Objects Inside of Array
+
+var ArrayObjects = [
+    {
+        "name": "Saket",
+        "age": 21,
+        "school": "ITUS"
+    },
+    {
+        "name": "Akshat",
+        "age": 25,
+        "school": "DPS"
+    },
+    {
+        "name": "Harshit",
+        "age": 34,
+        "school": "Nahi Gaya Hai"
+    }
+]
+
+console.log(ArrayObjects[2]['school'])
+console.log(ArrayObjects[2].school)
+
+ArrayObjects[1]['school'] = "Kabhi Nahi Jayega";
+console.log(ArrayObjects[1].school)
+
+// This similar method can be used to access a array inside of a object or a object inside of an array
